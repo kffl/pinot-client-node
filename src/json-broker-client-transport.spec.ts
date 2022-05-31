64116e152c8f49470e1e721188c3958091034641
Paweł Kuffel <pawel@kuffel.io>
2022-05-31 16:47:35 +0200

    test: additional error handlig test cases for broker and controller HTTP client transports

--- src/json-broker-client-transport.spec.ts
+++ src/json-broker-client-transport.spec.ts
@@ -1,9 +1,13 @@
-import { BrokerResponse } from "./broker-response.types";
-import { HttpPostFn, JsonBrokerClientTransport } from "./json-broker-client-transport";
+import { AxiosError } from "axios";
+import { JsonBrokerClientTransport } from "./json-broker-client-transport";
 
 describe("JsonBrokerClientTransport class", () => {
+    const mockHttpPostFn = jest.fn();
+    beforeEach(() => {
+        mockHttpPostFn.mockClear();
+    });
     it("should call the HttpPostFn", async () => {
-        const mockHttpPostFn: HttpPostFn<BrokerResponse> = jest.fn().mockResolvedValueOnce({ data: {} });
+        mockHttpPostFn.mockResolvedValueOnce({ data: {} });
         const transport = new JsonBrokerClientTransport(mockHttpPostFn);
         await transport.executeQuery("addr:8000", "query");
         expect(mockHttpPostFn).toHaveBeenCalledTimes(1);
@@ -16,5 +20,18 @@ describe("JsonBrokerClientTransport class", () => {
                 },
             }
         );
+    });
+    it("should throw an error with status code on HTTP error", async () => {
+        const errWithResponse = new AxiosError("message");
+        errWithResponse.response = { status: 500, data: {}, headers: {}, statusText: "", config: {} };
+        mockHttpPostFn.mockRejectedValueOnce(errWithResponse);
+        const transport = new JsonBrokerClientTransport(mockHttpPostFn);
+        await expect(transport.executeQuery("addr:8000", "query")).rejects.toThrowError("500");
+    });
+    it("should throw an error with message on other errors", async () => {
+        const errWithMessage = new AxiosError("sample message");
+        mockHttpPostFn.mockRejectedValueOnce(errWithMessage);
+        const transport = new JsonBrokerClientTransport(mockHttpPostFn);
+        await expect(transport.executeQuery("addr:8000", "query")).rejects.toThrowError("sample message");
     });
 });
--- src/json-controller-client-transport.spec.ts
+++ src/json-controller-client-transport.spec.ts
@@ -1,9 +1,13 @@
-import { ControllerResponse } from "./controller-response";
-import { HttpGetFn, JsonControllerClientTransport } from "./json-controller-client-transport";
+import { AxiosError } from "axios";
+import { JsonControllerClientTransport } from "./json-controller-client-transport";
 
 describe("JsonControllerClientTransport class", () => {
+    const mockHttpGetFn = jest.fn();
+    beforeEach(() => {
+        mockHttpGetFn.mockClear();
+    });
     it("should call the HttpGetFn", async () => {
-        const mockHttpGetFn: HttpGetFn<ControllerResponse> = jest.fn().mockResolvedValueOnce({ data: {} });
+        mockHttpGetFn.mockResolvedValueOnce({ data: {} });
         const transport = new JsonControllerClientTransport("controller:9000", mockHttpGetFn);
         const r = await transport.getTableToBrokerMapping();
         expect(mockHttpGetFn).toHaveBeenCalledTimes(1);
@@ -13,5 +17,18 @@ describe("JsonControllerClientTransport class", () => {
             },
         });
         expect(r).toEqual({});
+    });
+    it("should throw an error with status code on HTTP error", async () => {
+        const errWithResponse = new AxiosError("message");
+        errWithResponse.response = { status: 503, data: {}, headers: {}, statusText: "", config: {} };
+        mockHttpGetFn.mockRejectedValueOnce(errWithResponse);
+        const transport = new JsonControllerClientTransport("addr:9000", mockHttpGetFn);
+        await expect(transport.getTableToBrokerMapping()).rejects.toThrowError("503");
+    });
+    it("should throw an error with message on other errors", async () => {
+        const errWithMessage = new AxiosError("sample message");
+        mockHttpGetFn.mockRejectedValueOnce(errWithMessage);
+        const transport = new JsonControllerClientTransport("addr:9000", mockHttpGetFn);
+        await expect(transport.getTableToBrokerMapping()).rejects.toThrowError("sample message");
     });
 });
